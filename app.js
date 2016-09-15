e5fe01e48f1a6d6a80fb95cf25c392bdc81acd3f
YangBo <yang.wilby@gmail.com>
2016-09-15 14:35:36 +0200

    first graphql works

--- app.js
+++ app.js
@@ -18,13 +18,36 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+//app.use(bodyParser.json());
+//app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
+app.use(bodyParser.text({ type: 'application/graphql' }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+
+
+/**
+ * graphql
+ */
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
+var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+var root = { hello: () => 'Hello world!' };
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
